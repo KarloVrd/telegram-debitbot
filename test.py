45f58570aff74dd46727b4db081b8c6d6ca65f99
KarloVrd <karlo.vrdoljak@gmail.com>
2025-04-06 20:39:59 +0200

    clean up

--- test.py
+++
@@ -1,7 +0,0 @@
-# bucket sort function without comments
-
-def bucket_sort(arr):
-    buckets = [[_] for _ in range(10)]
-    for num in arr:
-        buckets[num].append(num)
-    return [num for bucket in buckets for num in bucket]
